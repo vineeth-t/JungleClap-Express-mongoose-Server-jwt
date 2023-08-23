671a10ac3ad3470111f83d63a21239f572cd227f
Pavan Vineethreddy Thaviti <120081880+rbrahmadevara@users.noreply.github.com>
2023-08-23 21:41:16 +0530

    fix: cors method added

--- index.js
+++ index.js
@@ -38,7 +38,12 @@ app.use('/user',users)
 app.use('/address',addresses)
 
 
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello Express app!')
